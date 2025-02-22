db5118680a2ddc0c8a674181ba4120d5d7ecbc55
salmaAla <salma.alaoui.pro@outlook.fr>
2025-02-22 06:35:14 +0100

    Implémentation du formatage XML pour les contacts au passage désolé j'oublie de commit à chaque fois

---
+++ src/main/java/exo1/FormatXML.java
@@ -0,0 +1,9 @@
+package exo1;
+
+public class FormatXML implements FormatAffichage {
+    @Override
+    public String formatter(Contact contact) {
+        return "<contact><nom>" + contact.getNom() + "</nom><telephone>" + contact.getNumero() + "</telephone>" +
+                (contact.getEmail() != null ? "<email>" + contact.getEmail() + "</email>" : "") + "</contact>";
+    }
+}

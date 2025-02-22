5715215a2e2f6097bdacd54b14d7b09abb232bba
salmaAla <salma.alaoui.pro@outlook.fr>
2025-02-22 03:55:11 +0100

    On définit les méthodes pour ajouter, supprimer et afficher des contacts.

--- src/main/java/exo1/ContactService.java
+++ src/main/java/exo1/ContactService.java
@@ -3,6 +3,5 @@ package exo1;
 public interface ContactService {
     void ajouteContact(Contact contact);
     void supprimeContact(Contact contact);
-    void envoiEmail(Contact contact, String message);
     void afficheContacts();
 }
